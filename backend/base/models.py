dfa5eaf3881147c4feb1653e090d7857ece4f08c
Guransh Singh <sguransh24@gmail.com>
2024-07-21 21:49:38 -0400

    Made the database

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True)        # if the parent gets deleted then the product is not deleted with this property
+                                                                                # null is false means that you can skip the user 
+    name= models.CharField(max_length=200, null=True, blank=True)       # null True means that you cannot skip this, but blank means it can be empty
+    # image=
+    brand= models.CharField(max_length=200, null=True, blank=True) 
+    category= models.CharField(max_length=200, null=True, blank=True) 
+    description= models.TextField(null=True, blank=True)
+    rating= models.DecimalField(max_digits=7, decimal_places=2)
+    numReviews= models.IntegerField(null=True, blank=True, default=0)
+    price= models.DecimalField(max_digits=7, decimal_places=2)
+    countInStock= models.IntegerField(null=True, blank=True, default=0)
+    createdAt= models.DateTimeField(auto_now_add=True)
+    _id= models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Review(models.Model):
+    product= models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name= models.CharField(max_length=200, null=True, blank=True)
+    rating= models.IntegerField(null=True, blank=True, default=0)
+    comment= models.TextField(null=True, blank=True)
+    _id= models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.rating)
+    
+
+class Order(models.Model):
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod= models.CharField(max_length=200, null=True, blank=True)
+    taxPrice= models.DecimalField(max_digits=7, decimal_places=2)
+    shippingPrice= models.DecimalField(max_digits=7, decimal_places=2)
+    totalPrice= models.DecimalField(max_digits=7, decimal_places=2)
+    isPaid= models.BooleanField(default=False)
+    paidAt= models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isDelivered= models.BooleanField(default=False)
+    deliveredAt=  models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdAt= models.DateTimeField(auto_now_add=True)
+    _id= models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.createdAt)
+    
+
+class OrderItem(models.Model):
+    product= models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order= models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name= models.CharField(max_length=200, null=True, blank=True)
+    qty= models.IntegerField(null=True, blank=True, default=0)
+    price= models.DecimalField(max_digits=7, decimal_places=2)
+    image= models.CharField(max_length=200, null=True, blank=True)      # This is a url
+    _id= models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.name)
+    
+
+class ShippingAddress(models.Model):
+    order= models.OneToOneField(Order, on_delete=models.CASCADE)   # Can have only one order, CASCADE deletes the address if the order is deleted
+    address= models.CharField(max_length=200, null=True, blank=True)
+    city= models.CharField(max_length=200, null=True, blank=True)
+    postalCode= models.CharField(max_length=200, null=True, blank=True)
+    country= models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice= models.DecimalField(max_digits=7, decimal_places=2)
+    _id= models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.address
